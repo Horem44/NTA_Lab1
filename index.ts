49e6996ef7cc48e936d633dc11fac4e8a3536362
Ilya Yegorov <ilyayegorov@Ilyas-MacBook-Air.local>
2023-03-13 22:30:48 +0200

    feat: numeric service

---
+++ index.ts
@@ -0,0 +1,13 @@
+import { NumericService } from "./numeric.service";
+
+const numeri = new NumericService();
+
+console.log(numeri.ifStrongPseudoprime(3825123056546413051, 9));
+
+// console.log(numeri.moduloHornerScheme(2, 15000, 113));
+
+
+
+
+
+
